d45a3ae8fe01aca92e49f6f19f18a73fc173b646
Mathias Silvero <fredy.silvero@genz.com.py>
2023-07-31 18:45:24 -0400

    cambios de login

--- src/components/TableList/utils/columns.jsx
+++ src/components/TableList/utils/columns.jsx
@@ -1,7 +1,7 @@
 
 
 const columns = [
-  { field: "name", headerName: "Nombre Completo", flex },
+  { field: "name", headerName: "Nombre Completo", width: 200 },
   { field: "col2", headerName: "Column 2", width: 150 }
 ]
 
